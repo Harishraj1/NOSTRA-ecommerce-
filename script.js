f3dd9486a9437f8ab8c3519b591a613d867d950c
Harishraj1 <harishrajd6@gmail.com>
2023-12-09 12:44:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var menubar = document.getElementById("menubar")
+var sidenav = document.querySelector(".sidenav")
+var closenav = document.getElementById("closenav")
+
+menubar.addEventListener("click",function(){
+    sidenav.style.left="0px"
+})
+closenav.addEventListener("click",function(){
+    sidenav.style.left="-40%"
+})
+
+// container
+
+var leftbtn = document.getElementById("left")
+var rightbtn = document.getElementById("right")
+var slidercontainer = document.querySelector(".slider-container")
+
+leftbtn.addEventListener("click",function(){
+    
+})
+rightbtn.addEventListener("click",function(){
+    
+})
